40b4b3da49146766987039ac70ef31dd78008e2c
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2020-12-27 14:53:47 -0500

    Empty project.

---
+++ Sudoku/Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Ghislain Leblanc on 2020-12-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
