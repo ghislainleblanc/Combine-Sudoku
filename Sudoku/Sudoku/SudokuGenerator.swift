64ca25cbd9e93205bab52f7f8984f125d81bf664
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2021-01-02 20:30:10 -0500

    Random generation.

--- Sudoku/Sudoku/SudokuGenerator.swift
+++ Sudoku/Sudoku/SudokuGenerator.swift
@@ -16,7 +16,7 @@ struct SudokuGenerator {
         for _ in 0...8 {
             var row = Array<Int>()
             for _ in 0...8 {
-                row.append(0)
+                row.append(Int.random(in: 1..<9))
             }
             grid.append(row)
         }
@@ -38,5 +38,51 @@ struct SudokuGenerator {
             [6, 7, 8,/**/ 9, 1, 2,/**/ 3, 4, 5],
             [9, 1, 2,/**/ 3, 4, 5,/**/ 6, 7, 8]
         ]
+    }
+
+    private func testGridSize(matrix: Matrix) -> Bool {
+        for i in 0...matrix.count - 1 {
+            if matrix[i].count != 9 {
+                return false
+            }
+        }
+        return true
+    }
+
+    private func testSquares(matrix: Matrix) -> Bool {
+        var failed = false
+        [0, 3, 6].forEach { (row) in
+            [0, 3, 6].forEach { (col) in
+                var digits = Set<Int>()
+
+                for i in 0...2 {
+                    for j in 0...2 {
+                        digits.insert(matrix[i + col][j + row])
+                    }
+                }
+
+                if digits.count != 9 {
+                    failed = true
+                    return
+                }
+            }
+        }
+
+        return failed
+    }
+
+    private func testRowsAndColumns(matrix: Matrix) -> Bool {
+        for i in 0...8 {
+            var rowDigits = Set<Int>()
+            var columnDigits = Set<Int>()
+            for j in 0...8 {
+                rowDigits.insert(matrix[i][j])
+                columnDigits.insert(matrix[j][i])
+            }
+            if rowDigits.count != 9 || columnDigits.count != 9 {
+                return false
+            }
+        }
+        return true
     }
 }
